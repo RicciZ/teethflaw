a470f225ebac889c0cd1a234775147e82554c3bc
RicciZ <RicciZZ@outlook.com>
2021-04-27 11:44:51 +0800

    modify default param

--- hole_prediction/main.py
+++ hole_prediction/main.py
@@ -235,7 +235,7 @@ if __name__ == "__main__":
 
     if args.all:
         args.exp_name = '_'.join(['{}[{}]'.format(k, v) for k, v in args.__dict__.items() if v != None])
-        args.tooth_ids = [11,12,13,14,15,16,17,21,22,23,24,25,26,27,28,31,32,33,34,35,36,37,38,41,42,43,44,45,46,47,48]
+        args.tooth_ids = [11,12,13,14,15,16,17,18,21,22,23,24,25,26,27,28,31,32,33,34,35,36,37,38,41,42,43,44,45,46,47,48]
     else:
         args.tooth_ids = [int(i) for i in args.tooth_ids.split(',')]
         args.exp_name = '_'.join(['{}[{}]'.format(k, v) for k, v in args.__dict__.items() if v != None])
